c80b582cc1026a2494c9d758cc2630490b6ccf13
Brian Brenner <bbrenne6@kent.edu>
2023-03-26 19:31:39 -0400

    added all debug

--- include/clox/debug.h
+++ include/clox/debug.h
@@ -3,8 +3,8 @@
 
 #include "chunk.h"
 
-void disassemble_chunk(Chunk* chunk, const char* name);
-int disassemble_instruction(Chunk* chunk, int offset);
+void disassemble_chunk(Chunk *chunk, const char *name);
+int disassemble_instruction(Chunk *chunk, int offset);
+static int simple_instruction(const char* name, int offset);
 
 #endif
-
