d186331f43a4eb26cca6e55ef343ad1f3c70c6cf
Brian Brenner <bbrenne6@kent.edu>
2023-03-26 19:18:46 -0400

    added debug

--- src/main.c
+++ src/main.c
@@ -1,10 +1,13 @@
-#include "chunk.h"
-#include "common.h"
+#include "../include/clox/chunk.h"
+#include "../include/clox/common.h"
+#include "../include/clox/debug.h"
 
 int main(int args, const char *argv[]) {
   Chunk chunk;
   init_chunk(&chunk);
   write_chunk(&chunk, OP_RETURN);
   free_chunk(&chunk);
+
+  disassemble_chunk(&chunk, "chunk test");
   return 0;
 }
